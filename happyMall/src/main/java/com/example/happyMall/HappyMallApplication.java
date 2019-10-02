77978316e05e7cda5117a09fa36d6efc7211b67c
Summer <44349698+ShuangZeng@users.noreply.github.com>
2019-10-02 12:00:33 -0500

    initial file

---
+++ happyMall/src/main/java/com/example/happyMall/HappyMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.happyMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyMallApplication.class, args);
+	}
+
+}
