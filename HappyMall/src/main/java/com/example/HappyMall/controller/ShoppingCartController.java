7a74306e4e266c1b8885bf1ad2e091dd7e867d82
vanthao91 <43080520+vanthao91@users.noreply.github.com>
2019-10-14 01:38:25 -0500

    Shopping cart

--- HappyMall/src/main/java/com/example/HappyMall/controller/ShoppingCartController.java
+++ HappyMall/src/main/java/com/example/HappyMall/controller/ShoppingCartController.java
@@ -1,14 +1,34 @@
 package com.example.HappyMall.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.HappyMall.domain.EditAddress;
 
 @Controller
 public class ShoppingCartController {
 
-	@GetMapping("/")
-	public String getShoppingCart()
+	@GetMapping("/shoppingcart")
+	public String getShoppingCart(Model model)
 	{
+		model.addAttribute("editAddress", new EditAddress());
 		return "shoppingcart";
 	}
+	
+//	@PostMapping("/")
+//	public String saveNewAddress(@ModelAttribute @Valid EditAddress editAddress, BindingResult rs, RedirectAttributes ra, Model model)
+//	{
+//		if (rs.hasErrors()) {
+//			return "shoppingcart";
+//		} else {
+//			return "shoppingcart";
+//		}
+//	}
 }
