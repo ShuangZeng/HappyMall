774bc6768f53a08336feb62395b4796f238b3350
ShuangZeng <zengshuang.dl@gmail.com>
2019-10-15 12:19:23 -0500

    payment management

--- HappyMall/src/main/java/com/example/HappyMall/controller/UserMgmtController.java
+++ HappyMall/src/main/java/com/example/HappyMall/controller/UserMgmtController.java
@@ -1,13 +1,36 @@
 package com.example.HappyMall.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.HappyMall.domain.User;
+import com.example.HappyMall.service.UserService;
 
 @Controller
 public class UserMgmtController {
+	
+	@Autowired
+	private UserService userService;
+	
+	
 	@RequestMapping(value = "/admin/usermgmt")
-	public String getShoppingCart()
+	public String getUserMgmt(Model model, Authentication authentication)
 	{
+		List<User> users = userService.findAllUsers();
+		model.addAttribute("userList",users);
 		return "UserMgmt";
 	}
+	//TODO
+	@RequestMapping(value = "/admin/editUser")
+	public String editUser()
+	{
+		
+		return "editUser";
+	}
+	
 }
