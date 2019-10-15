0fcdf62c9a2a9171e600144659207340b95ccc3a
mHeakal <heakalmohammed@gmail.com>
2019-10-15 15:09:51 -0500

    product controller fix and service minor fix

---
+++ HappyMall/src/main/java/com/example/HappyMall/repository/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.example.HappyMall.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.jboss.logging.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.HappyMall.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+	
+Optional<Product> findByname(String name);
+	
+	List<Product> findProductsByname(String name);
+	
+//	@Query("from Product where vendor_id=:vendor_id")
+//	List<Product> findByVendorNumber(@Param(value="vendor_id") Integer vendorNumber);
+//	
+//	@Query("delete from Product where id=:product_id")
+//	void deleteProduct(@Param(value="product_id") Integer productId);
+
+}
