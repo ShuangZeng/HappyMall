80c29312cbe5ebf44af844bbdcca55fe8dfac05c
mHeakal <heakalmohammed@gmail.com>
2019-10-17 14:02:29 -0500

    product fix, added UI

--- HappyMall/src/main/java/com/example/HappyMall/controller/ProductController.java
+++ HappyMall/src/main/java/com/example/HappyMall/controller/ProductController.java
@@ -1,44 +1,38 @@
 package com.example.HappyMall.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.HappyMall.domain.Product;
 import com.example.HappyMall.service.ProductService;
 
 @Controller
-@RequestMapping("/products")
+@RequestMapping
 public class ProductController {
 
 	@Autowired
 	ProductService productService;
 
-	@GetMapping("/")
-	public String listProducts(Model model) {
-		// get the product list by vendor
-		return "product/productsList";
+	@GetMapping(value = "/admin/products")
+	public String getProductsList(Model model, Authentication authentication) {
+		model.addAttribute("productList", productService.findAllProducts());
+		return "productsList";
 	}
 
-	@GetMapping(value = { "/updateProduct", "/updateProduct/{id}" })
-	public String updateProduct(Model model, @PathVariable(required = false, name = "id") Integer id) {
-		//updating product
-		return "product/updateProduct";
+
+	@GetMapping(value = "/admin/update")
+	public String getUpdateProducts(Model model, Authentication authentication) {
+		model.addAttribute("productList", productService.findAllProducts());
+		return "updateProducts";
 	}
 	
-	@GetMapping(value = "/allproducts")
-	public String getAllProducts(Model model) {
-		System.out.println("Get All products for admin..");
-		model.addAttribute("productsList", productService.findAllProducts());
-		return "product/allProducts";
+	@GetMapping(value = "/admin/editProduct")
+	public String editProducts(Model model, Authentication authentication, @RequestParam Integer productId) {
+		model.addAttribute("product", productService.getProduct(productId));
+		return "editProduct";
 	}
 }
--- HappyMall/src/main/java/com/example/HappyMall/service/ProductService.java
+++ HappyMall/src/main/java/com/example/HappyMall/service/ProductService.java
@@ -1,34 +1,16 @@
 package com.example.HappyMall.service;
 
 import java.util.List;
-import java.util.Optional;
-
 import com.example.HappyMall.domain.Product;
-import com.example.HappyMall.repository.ProductRepository;
-
 
 public interface ProductService{
 	
 	void addProduct(Product product);
 	Product approveProduct(Product product);
 	Product blockProduct(Product product);
-//	Optional<Product> getProduct(Integer id);
-
-//	void deleteProduct(Integer id);
-
 	List<Product> findAllProducts();
-
-//	void setStock(String productNumber, int quantity, String locationCode);
-//
-//	Stock getStock(String productNumber);
-
 	List<Product> findProductsByProductName(String name);
-
-//	Product getProductByProductNumber(String productNumber);
-
 	List<Product> findProductsByVendor(int vendorNumber);
-
-
 	Product getProduct(Integer id);
 	List<Product> findProductsByvendor_id(int vendorId);
 
