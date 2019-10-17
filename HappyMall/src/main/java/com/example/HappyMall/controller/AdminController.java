7ca69d51c4afb2e496366e4cd3ff1d789731e01b
ShuangZeng <zengshuang.dl@gmail.com>
2019-10-17 18:09:22 -0500

    header

--- HappyMall/src/main/java/com/example/HappyMall/controller/AdminController.java
+++ HappyMall/src/main/java/com/example/HappyMall/controller/AdminController.java
@@ -1,14 +1,21 @@
 package com.example.HappyMall.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.example.HappyMall.domain.User;
 
 @Controller
+@SessionAttributes ({"user"})
 public class AdminController {
 
 	@RequestMapping(value = "/admin")
-	public String getShoppingCart()
+	public String getShoppingCart(Model model)
 	{
+		User user = (User)model.asMap().get("user");
+		model.addAttribute("user", user);
 		return "admin";
 	}
 }
