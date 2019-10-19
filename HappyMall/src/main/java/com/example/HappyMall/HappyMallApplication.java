b4f34daf1b666327d7b1b46096e46371bec56ee6
anneguimaraes <56128151+anneguimaraes@users.noreply.github.com>
2019-10-19 15:01:53 -0500

    Anne: fixed fetching from webservices

--- HappyMall/src/main/java/com/example/HappyMall/HappyMallApplication.java
+++ HappyMall/src/main/java/com/example/HappyMall/HappyMallApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-//@ComponentScan(basePackages={"com.example.HappyMall.controller"})
+@ComponentScan(basePackages={"com.example.HappyMall"})
 public class HappyMallApplication {
 
 	public static void main(String[] args) {
