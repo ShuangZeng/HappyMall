0bf9c147ca253f705a03180083ca60a409cf0373
mHeakal <heakalmohammed@gmail.com>
2019-10-20 22:16:53 -0500

    Minor fix raised to the power of 2

--- HappyMall/src/main/java/com/example/HappyMall/controller/SearchController.java
+++ HappyMall/src/main/java/com/example/HappyMall/controller/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.HappyMall.domain.Product;
 import com.example.HappyMall.service.ProductService;
 
 @Controller
@@ -20,6 +21,7 @@ public class SearchController {
 
 	@GetMapping(value = "/searchProducts")
 	public String searchProduct(Model model) {
+		model.addAttribute("product", new Product());
 		return "searchProducts";
 	}
 
